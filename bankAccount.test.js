38f5df3d504e5daa53177bd69742580f30154592
Caroline Evans <carolineevans261@gmail.com>
2023-06-19 15:09:55 +0100

    added README

--- bankAccount.test.js
+++ bankAccount.test.js
@@ -37,7 +37,6 @@ describe('BankAccount', () => {
         account.withdraw(500, "14/01/2023");
         statement = account.printStatement();
         expectedText = "date || credit || debit || balance\n14/01/2023 ||  || 500.00 || 2500.00\n13/01/2023 || 2000.00 ||  || 3000.00\n10/01/2023 || 1000.00 ||  || 1000.00"
-        console.log(statement)
         expect(statement).toEqual(expectedText);
     });
 
