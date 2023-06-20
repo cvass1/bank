2aecbd198aafac4e32301c60bba0deeb6771cf9b
Caroline Evans <carolineevans261@gmail.com>
2023-06-20 14:00:58 +0100

    updated integration test to mock Date

--- integration.test.js
+++ integration.test.js
@@ -1,7 +1,17 @@
 const BankAccount = require("./bankAccount");
 const Transaction = require("./transaction");
 
-describe('BankAccount', () => {
+describe('Integration', () => {
+    beforeAll(() => {
+        originalDate = global.Date;
+        const mockDate = new Date('2023-06-01T00:00:00');
+        global.Date = jest.fn(() => mockDate);
+    });
+    
+    afterAll(() => {
+        global.Date = originalDate;
+    });
+
 
     test(('initially returns an empty bank statement'), () =>{
         const account = new BankAccount(Transaction);
@@ -11,7 +21,7 @@ describe('BankAccount', () => {
 
     test(('adds a deposit to the balance and displays transaction on the statement'), () =>{
         const account = new BankAccount(Transaction);
-        account.deposit(1000, "01/06/2023")
+        account.deposit(1000)
         statement = account.printStatement();
         expectedText = "date || credit || debit || balance\n01/06/2023 || 1000.00 ||  || 1000.00"
         expect(statement).toEqual(expectedText);
@@ -19,20 +29,20 @@ describe('BankAccount', () => {
 
     test(('takes away from the balance and displays transaction on the statement in reverse time order'), () =>{
         const account = new BankAccount(Transaction);
-        account.deposit(1000, "01/06/2023");
-        account.withdraw(300, "02/06/2023");
+        account.deposit(1000);
+        account.withdraw(300);
         statement = account.printStatement();
-        expectedText = "date || credit || debit || balance\n02/06/2023 ||  || 300.00 || 700.00\n01/06/2023 || 1000.00 ||  || 1000.00"
+        expectedText = "date || credit || debit || balance\n01/06/2023 ||  || 300.00 || 700.00\n01/06/2023 || 1000.00 ||  || 1000.00"
         expect(statement).toEqual(expectedText);
     });
 
     test(('shows multiple transactions in reverse time order'), () =>{
         const account = new BankAccount(Transaction);
-        account.deposit(1000, "10/01/2023");
-        account.deposit(2000, "13/01/2023");
-        account.withdraw(500, "14/01/2023");
+        account.deposit(1000);
+        account.deposit(2000);
+        account.withdraw(500);
         statement = account.printStatement();
-        expectedText = "date || credit || debit || balance\n14/01/2023 ||  || 500.00 || 2500.00\n13/01/2023 || 2000.00 ||  || 3000.00\n10/01/2023 || 1000.00 ||  || 1000.00"
+        expectedText = "date || credit || debit || balance\n01/06/2023 ||  || 500.00 || 2500.00\n01/06/2023 || 2000.00 ||  || 3000.00\n01/06/2023 || 1000.00 ||  || 1000.00"
         expect(statement).toEqual(expectedText);
     });
 
